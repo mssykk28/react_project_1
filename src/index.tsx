83ed2ee5dcb43060392a073505429a616180746d
masashi <mss.nt.2828@gmail.com>
2022-03-29 19:40:57 +0900

    日本語訳の追加

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//root のIDをもつDOM要素（コンテナ）に <App /> をレンダリングしている。
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +12,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// アプリでパフォーマンス測定を開始したい場合は、関数を渡します。
+// 結果を記録する（例：reportWebVitals(console.log))
+// または解析エンドポイントに送信します。詳細はこちら： https://bit.ly/CRA-vitals
 reportWebVitals();
