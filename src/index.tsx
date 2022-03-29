bc34ca2b3b1f85792c00de2430ea57578e6772d3
masashi <mss.nt.2828@gmail.com>
2022-03-29 21:22:43 +0900

    firebaseにデプロイできるように設定

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+      <h1>mss_deploy</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
